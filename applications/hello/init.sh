f650663ffb9c54279c2732da86719d9957505ee5
Chris Fox <chris.f@quali.com>
2021-03-17 10:22:13 -0700

    changing port

--- applications/hello/init.sh
+++ applications/hello/init.sh
@@ -7,3 +7,12 @@ printenv > /var/log/colony-vars-"$(basename "$BASH_SOURCE" .sh)".txt
 
 apt-get update -y
 
+apt-get install python3 -y
+apt-get install python3-pip -y
+
+mkdir $ARTIFACTS_PATH/drop
+tar -xvf $ARTIFACTS_PATH/artifacts.tar.gz -C $ARTIFACTS_PATH/drop/
+
+python3 -m pip install -r $ARTIFACTS_PATH/drop/Website/requirements.txt
+nohup python3 $ARTIFACTS_PATH/drop/Website/__main__.py > webapp.txt 2>&1 &
+
