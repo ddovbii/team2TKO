c10585949a5239139cca93f4d96eacab80fbd590
Omri Avraham <omritk@gmail.com>
2021-03-16 18:15:42 -0600

    added pip

--- applications/hello-simple/init.sh
+++ applications/hello-simple/init.sh
@@ -8,6 +8,7 @@ printenv > /var/log/colony-vars-"$(basename "$BASH_SOURCE" .sh)".txt
 apt-get update -y
 
 apt-get install python3 -y
+apt-get install python3-pip -y
 
 mkdir $ARTIFACTS_PATH/drop
 tar -xvf $ARTIFACTS_PATH/artifacts.tar.gz -C $ARTIFACTS_PATH/drop/
