1c3c6890ee09696a2f93c6606fadd5f889089730
Omri Avraham <omritk@gmail.com>
2021-03-16 23:09:28 -0600

    added connection string input support

--- Website/__main__.py
+++ Website/__main__.py
@@ -1,12 +1,19 @@
 from flask import Flask
 import socket
+import os
+
+CON_STR = os.getenv("connection_string", None)
 
 webapp = Flask("Team2")
 
 @webapp.route("/")
 def print_private_ip():
+    msg = ""
     ip = socket.gethostbyname(socket.gethostname())
-    return "My Private IP is: {}".format(ip)
+    msg += "My Private IP is: {}".format(ip)
+    if CON_STR:
+        msg += "\nConnection String: {}".format(CON_STR)
+    return msg
 
 webapp.run("0.0.0.0", 80)
 
