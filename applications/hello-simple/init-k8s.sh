219782e6fd9b15dcb86f88acf66e26ed60c75340
Chris Fox <chris.f@quali.com>
2021-03-17 11:05:09 -0700

    k8s fix

---
+++ applications/hello-simple/init-k8s.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -e  # Stop Script on Error
+
+# save all env for debugging
+printenv > /var/log/colony-vars-"$(basename "$BASH_SOURCE" .sh)".txt
+
+apt-get update -y
+
+apt-get install python3 -y
+apt-get install python3-pip -y
+
+mkdir $ARTIFACTS_PATH/drop
+tar -xvf $ARTIFACTS_PATH/artifacts.tar.gz -C $ARTIFACTS_PATH/drop/
+
+python3 -m pip install -r $ARTIFACTS_PATH/drop/Website/requirements.txt
+python3 $ARTIFACTS_PATH/drop/Website/__main__.py > webapp.txt
+
