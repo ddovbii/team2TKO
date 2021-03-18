64f4accc5a4f2f4d5c715511d60d8d61a72ae1cc
Chris Fox <chris.f@quali.com>
2021-03-18 14:43:27 -0700

    addin health check to all apps

---
+++ applications/hello-simple/healthcheck.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+PUBLIC_ENDPOINT="http://${PUBLIC_ADDRESS}"
+TEXT_TO_CHECK='My Private IP is bob ross'
+
+# Create retry function
+function retry {
+  while :
+  do
+    RESPONSE=$(curl -L -k "${@}")
+    STATUSCODE=$(curl -L -o /dev/null -s -w "%{http_code}\n" -k "${@}")
+    if [[ "$STATUSCODE" -ne "000" && "$STATUSCODE" -eq "200" ]] 
+    then
+      if [[ "$RESPONSE" == *"$TEXT_TO_CHECK"* ]]
+      then
+        echo "External ALB ${@} is healthy and has valid text. Statuscode: ${STATUSCODE}" && break
+      fi
+        echo "External ALB ${@} is healthy but has invalid text. Statuscode: ${STATUSCODE}" && exit 1
+    fi
+      echo "External ALB ${@} is unhealthy. Statuscode: ${STATUSCODE}" && sleep 20
+  done
+}
+
+
+for ALB in $PUBLIC_ENDPOINT;
+do
+  retry $ALB
+done
