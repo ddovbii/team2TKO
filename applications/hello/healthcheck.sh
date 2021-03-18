4a39024107f6c70347ba1c3328afbd80e3b55b2a
Chris Fox <chris.f@quali.com>
2021-03-18 12:34:38 -0700

    health check

--- applications/hello/healthcheck.sh
+++ applications/hello/healthcheck.sh
@@ -1,26 +1,28 @@
 #!/bin/bash
 
-ALB_STS="https://cst${SANDBOX_ID}.${domain_name}/STS"
-ALB_InformRMS="https://cst${SANDBOX_ID}.${domain_name}/InformRMS"
-# ALB_InformRMSHost="https://cst${SANDBOX_ID}.${domain_name}/InformRMSHost"
-# ALB_optimus="https://cst${SANDBOX_ID}.${domain_name}/optimus"
-
+PUBLIC_ENDPOINT="http://${PUBLIC_ADDRESS}"
+TEXT_TO_CHECK='My Private IP is'
 
 # Create retry function
 function retry {
   while :
   do
+    RESPONSE=$(curl -k "${@}")
     STATUSCODE=$(curl -o /dev/null -s -w "%{http_code}\n" -k "${@}")
-    if [[ "$STATUSCODE" -ne "000" && "$STATUSCODE" -le "399" ]] 
+    if [[ "$STATUSCODE" -ne "000" && "$STATUSCODE" -eq "200" ]] 
     then
-      echo "External ALB ${@} is healthy. Statuscode: ${STATUSCODE}" && break
+      if [[ "$RESPONSE" == *"$TEXT_TO_CHECK"* ]]
+      then
+      echo "External ALB ${@} is healthy and has valid text. Statuscode: ${STATUSCODE}" && break
+      fi
+      echo "External ALB ${@} is healthy but has invalid text. Statuscode: ${STATUSCODE}" && exit 1
     fi
       echo "External ALB ${@} is unhealthy. Statuscode: ${STATUSCODE}" && sleep 20
   done
 }
 
 
-for ALB in $ALB_STS $ALB_InformRMS; # $ALB_InformRMSHost $ALB_optimus
+for ALB in $PUBLIC_ENDPOINT;
 do
   retry $ALB
 done
