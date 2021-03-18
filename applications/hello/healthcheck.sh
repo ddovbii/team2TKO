d9693498e4bc343f20b686186666c8e81c0af00f
Chris Fox <chris.f@quali.com>
2021-03-18 14:29:45 -0700

    health check

--- applications/hello/healthcheck.sh
+++ applications/hello/healthcheck.sh
@@ -11,7 +11,7 @@ function retry {
   do
     RESPONSE=$(curl -L -k "${@}")
     STATUSCODE=$(curl -L -o /dev/null -s -w "%{http_code}\n" -k "${@}")
-    if [[ "$STATUSCODE" -ne "000" && "$STATUSCODE" -eq "409" ]] 
+    if [[ "$STATUSCODE" -ne "000" && "$STATUSCODE" -eq "200" ]] 
     then
       if [[ "$RESPONSE" == *"$TEXT_TO_CHECK"* ]]
       then
