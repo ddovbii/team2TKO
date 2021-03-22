9d70799264abebde381505c12eeb2183f07041df
Omri Avraham <omritk@gmail.com>
2021-03-22 12:02:53 -0600

    Update __main__.py

--- Website/__main__.py
+++ Website/__main__.py
@@ -13,6 +13,7 @@ def print_private_ip():
     msg += "My Private IP is: {}".format(ip)
     if CON_STR:
         msg += "\nConnection String: {}".format(CON_STR)
+    msg += "team2"
     return msg
 
 webapp.run("0.0.0.0", 80)
